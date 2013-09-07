11d0ce33bdd70ebefead204b609e5ef29dd5b390
Aditya <adityadalal924@gmail.com>
2013-09-07 18:53:11 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+from hackpack.app import app
+app.debug = True
+
+# If PORT not specified by environment, assume development config.
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    if port == 5000:
+        app.debug = True
+    app.run(host='0.0.0.0', port=port)
---
+++ hackpack/foursquare.py
@@ -0,0 +1,39 @@
+import json
+import urllib2
+import urllib
+from pprint import pprint
+
+def CompressURL(url):
+	apiurl = "http://to.ly/api.php?longurl="
+	quoted = urllib.quote_plus(url)
+	shorturl = urllib.urlopen(apiurl + quoted).read()
+	return shorturl
+
+def parseBody(string):
+	# string = "philadelphia, pa coffee"
+	splitted = string.split(" ")
+	location = splitted[0] + " " + splitted[1]
+	category = splitted[2]
+	data = [location, category]
+	return data
+
+
+def search(location, category):
+	location = urllib.quote(location)
+	category = urllib.quote(category)
+	url = "https://api.foursquare.com/v2/venues/explore?mode=url&near=" + location + "&query=" + category + "&client_id=QD350G42SFQ2PZUP3JLWAF5U453UEKX3RFDJOFKUIRUESXDU&limit=10&client_secret=4BFGFNV4M0IP0J1GIMUKKIFTZ5VDJTYCCGXECRCWAND3TZSP&v=20130907"
+	print url
+	data = json.load(urllib2.urlopen(url))
+	compressedURL = CompressURL(data["response"]["groups"][0]["items"][0]["venue"]["canonicalUrl"])
+
+	reply= [data["response"]["groups"][0]["items"][0]["venue"]["name"], compressedURL]
+	return reply
+
+
+def main():
+#	data = search("New%20Brunswick%2C%20NJ", "coffee")
+	data = search("New Brunswick, NJ", "coffee")
+	print data[0]
+	print data[1]
+if __name__ == "__main__":
+	main()
