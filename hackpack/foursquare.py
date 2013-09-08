776e35f3f492a9b1b9da300a8ecb1a52c242adc2
Aditya <adityadalal924@gmail.com>
2013-09-08 11:36:24 -0400

    fix fail

--- hackpack/foursquare.py
+++ hackpack/foursquare.py
@@ -19,14 +19,14 @@ def parseBody(string):
 def search(location, category):
 	location = urllib.quote(location)
 	category = urllib.quote(category)
-	url = "https://api.foursquare.com/v2/venues/explore?mode=url&near=" + location + "&query=" + category + "&client_id=QD350G42SFQ2PZUP3JLWAF5U453UEKX3RFDJOFKUIRUESXDU&limit=10&client_secret=4BFGFNV4M0IP0J1GIMUKKIFTZ5VDJTYCCGXECRCWAND3TZSP&v=20130907"
+	url = "https://api.foursquare.com/v2/venues/explore?mode=url&near=" + location + "&query=" + category + "&client_id=QD350G42SFQ2PZUP3JLWAF5U453UEKX3RFDJOFKUIRUESXDU&limit=10&client_secret=4BFGFNV4M0IP0J1GIMUKKIFTZ5VDJTYCCGXECRCWAND3TZSP&v=20130908"
 	# get JSON
 	data = json.load(urllib2.urlopen(url))
 	venue_name = data["response"]["groups"][0]["items"][0]["venue"]["name"]
 	venue_address = data["response"]["groups"][0]["items"][0]["venue"]["location"]["address"] + " " + data["response"]["groups"][0]["items"][0]["venue"]["location"]["crossStreet"]
 	compressedURL = CompressURL(data["response"]["groups"][0]["items"][0]["venue"]["canonicalUrl"])
 
-	reply= [venue_name, venue_address, compressedURL]
+	reply = [venue_name, venue_address, compressedURL]
 	return reply
 
 
